0197e29d86c2cd6042ac0f8c0ad54807fe974529
An Duong <an.duongx@gmail.com>
2022-09-10 18:30:33 -0400

    Add jsdoc for createCombinedStoresHook

--- src/react.ts
+++ src/react.ts
@@ -34,6 +34,10 @@ export function createCombinedStoresHook<
   let state = mergeBy(storeMap, (store: Store<M>) => store.getState()) as M;
   const listeners = new Set<TListener>();
 
+  /**
+   * It takes the previous state, the store key, and the action name, and then it loops through all the
+   * listeners and calls them with the current state, the previous state, and the action key
+   */
   function publish(previousState: M, storeK: string, actionName: string) {
     const actionK = `${storeK}/${actionName}`;
     for (const listener of Array.from(listeners)) {
@@ -41,6 +45,10 @@ export function createCombinedStoresHook<
     }
   }
 
+  /**
+   * It takes the store's key, the action's name, and the store's state, and then updates the state
+   * object with the new store's state
+   */
   function updateState(
     storeK: string,
     actionName: string,
@@ -56,7 +64,10 @@ export function createCombinedStoresHook<
     publish(previous, storeK, actionName);
   }
 
-  function subscribeToToStores() {
+  /**
+   * It subscribes to all the stores in the storeMap and updates the state of the store in the storeMap
+   */
+  function createStoreSubscriptions() {
     for (const storeK of Object.keys(storeMap)) {
       storeMap[storeK]?.subscribe((storeState, _, actionName) => {
         updateState(storeK, actionName as string, storeState);
@@ -64,15 +75,25 @@ export function createCombinedStoresHook<
     }
   }
 
+  /**
+   * Subscribe takes a listener and returns a function that removes that listener.
+   */
   function subscribe(listener: Listener<M, string>) {
     listeners.add(listener);
     return () => listeners.delete(listener);
   }
 
+  /**
+   * It returns the value of the variable state.
+   */
   function getState() {
     return state;
   }
 
+  /**
+   * It returns a value that is derived from the state of the store, and it will automatically update
+   * whenever the state of the store changes
+   */
   function makeHook(selector: Selector<M>, equalityFn?: EqualityFn<M>) {
     const value = useSyncExternalStoreWithSelector(
       subscribe,
@@ -86,19 +107,30 @@ export function createCombinedStoresHook<
     return value;
   }
 
+  /**
+   * `select` is a function that takes a selector function and an optional equality function and returns
+   * value of the selector function
+   */
   function select<T>(selector: Selector<M, T>, equalityFn?: EqualityFn<M>): T {
     return makeHook(selector, equalityFn) as unknown as T;
   }
 
+  /**
+   * It takes a list of selectors and returns a list of selectors that have been modified to call a
+   * hook
+   */
   function modSelectors() {
     return mergeBy(selectors, (selector) => () => makeHook(selector));
   }
 
+  /**
+   * It takes a map of stores and returns a map of actions
+   */
   function modActions(): { [K in keyof S]: S[K]['actions'] } {
     return mergeBy(storeMap, (store) => store.actions);
   }
 
-  subscribeToToStores();
+  createStoreSubscriptions();
 
   return {
     useSelect: select,
@@ -117,6 +149,9 @@ export function createStoreHook<
   const store = isStore(_store) ? _store : createStore(_store);
   const { selectors } = options ?? {};
 
+  /**
+   * It returns a value that is always in sync with the value returned by the selector function
+   */
   function makeHook(selector: Selector<State>, equalityFn?: EqualityFn<State>) {
     const value = useSyncExternalStoreWithSelector(
       store.subscribe,
@@ -130,6 +165,9 @@ export function createStoreHook<
     return value;
   }
 
+  /**
+   * "It returns a hook that returns the value of the selector function."
+   */
   function select<T>(
     selector: Selector<State, T>,
     equalityFn?: EqualityFn<State>,
